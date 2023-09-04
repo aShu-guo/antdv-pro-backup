ad94b48261e22a4ca0d6e4024bfd52ed59c19b9a
aShu-guo <guochengli97@163.com>
2023-09-04 21:13:00 +0800

    fix: collapsed trigger

--- src/layouts/components/menu/index.vue
+++ src/layouts/components/menu/index.vue
@@ -23,7 +23,7 @@ const menuTheme = computed(() => {
 <template>
   <a-menu
     :selected-keys="selectedKeys"
-    :open-keys="openKeys"
+    :open-keys="collapsed ? [] : openKeys"
     :mode="layout === 'top' && !isMobile ? 'horizontal' : 'inline'"
     :theme="menuTheme"
     :collapsed="collapsed"
